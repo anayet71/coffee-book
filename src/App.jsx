fc16f355af44501155f4260f226d513f9834a6af
anayet71 <ariyananayet@gmail.com>
2025-02-14 20:27:41 +0600

    jsx file setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
@@ -12,6 +10,7 @@ function App() {
       <p className="read-the-docs text-6xl ">
         Click on the Vite and React logos to learn more
       </p>
+      <button className='btn btn-primary'>Hi</button>
     </>
   )
 }
